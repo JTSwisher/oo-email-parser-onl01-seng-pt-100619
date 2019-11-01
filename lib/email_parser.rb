e39fe7a7c777ec3db7e0df1b0b2e7cdc8a670cc5
Jeff Swisher <jefftswisher@gmail.com>
2019-11-01 19:59:38 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -6,11 +6,11 @@ class EmailAddressParser
   attr_accessor :email_address
   
   def initialize(email_address)
-    @email = email_address
+    @email_address = email_address
   end
   
   def parse
-    email.split(/, | / ).uniq
+    email_address.split(/, | / ).uniq
   end 
   
 end 
